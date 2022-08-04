bf67b15b6f88c607e0d782f2296f1631d3a81e6a
Xnanga <jpeutherer1@hotmail.co.uk>
2022-08-04 14:51:35 +0100

    Include backend changes in commit

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -4,6 +4,7 @@ const employeeController = require('../controllers/employeeController');
 const router = express.Router();
 
 router.get('/api/get-all-employees', employeeController.employee_get_all);
+router.get('/api/get-employee&id=:id', employeeController.employee_get_one);
 router.post('/api/create-employee', employeeController.employee_create);
 router.delete(
   '/api/delete-employee&id=:id',
